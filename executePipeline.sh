f964a03f983fdc3f60f39c3a9242f66c2fb488de
Mark Cheung <mcheung@redhat.com>
2021-12-09 11:37:45 -0500

    Moved pipeline run to the tekton directory

--- executePipeline.sh
+++ executePipeline.sh
@@ -3,4 +3,4 @@
 
 echo "Make sure to test the sonar is ready and listening for requests.  Otherwise, the mvn build will fail"
 
-oc create -f build-and-rollout-pipeline-run.yaml
+oc create -f tekton/build-and-rollout-pipeline-run.yaml
